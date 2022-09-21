f4f688bff5e358a61ef05eff48a51b6e54f025c0
Parthiban <Talktoparthi@gmail.com>
2022-09-21 17:51:55 +0530

    sidebar lists updated

--- src/AComponents/ALayout/BSideBar/BSideBarLists.js
+++ src/AComponents/ALayout/BSideBar/BSideBarLists.js
@@ -5,6 +5,8 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import Divider from "@mui/material/Divider";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function BSideBarLists() {
   return (
@@ -15,6 +17,15 @@ function BSideBarLists() {
             <CurrencyRupeeIcon />
           </ListItemIcon>
           <ListItemText primary="Edit Exchange Rate" />
+        </ListItemButton>
+      </ListItem>
+      <Divider />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Manage Users" />
         </ListItemButton>
       </ListItem>
     </List>
