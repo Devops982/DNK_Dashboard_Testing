b4e3fa9b6f79b43d38ecf86ea146cfabebb0bf47
Parthiban <Talktoparthi@gmail.com>
2022-09-21 12:16:18 +0530

    DNK Dashboard project started with appbar Material UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import AHeader from "./AComponents/ALayout/AHeader/AHeader";
+
+function App() {
+  return (
+    <div>
+      <AHeader />
+      <h1>Hello, World</h1>
+      <p>I am writing JSX</p>
+    </div>
+  );
+}
+
+export default App;
