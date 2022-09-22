8c18f0bb93ff08551fe8295655c298a36b8d276b
Parthiban <Talktoparthi@gmail.com>
2022-09-22 10:53:19 +0530

    Table edited

--- src/AComponents/ALayout/CMiddleBar/BCurrencyExchangeRate.js
+++ src/AComponents/ALayout/CMiddleBar/BCurrencyExchangeRate.js
@@ -6,6 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Typography } from "@mui/material";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -22,15 +23,14 @@ const rows = [
 function BCurrencyExchangeRate() {
   return (
     <div>
+      <Paper> <Typography variant="h5" align="center" sx={{fontWeight: 'bold'}}>Update Currency Values</Typography></Paper>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Curreny Name</TableCell>
+              <TableCell align="right">Indian Rupee Value</TableCell>
+              <TableCell align="right">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -44,8 +44,6 @@ function BCurrencyExchangeRate() {
                 </TableCell>
                 <TableCell align="right">{row.calories}</TableCell>
                 <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
               </TableRow>
             ))}
           </TableBody>
